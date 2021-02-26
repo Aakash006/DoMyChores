db153c0492db8b31caebd6392ef27dc8da8c719c
Aakash006 <aakashpatel141@gmail.com>
2021-02-26 15:40:14 -0500

    initial commit

---
+++ src/components/Login/Login.js
@@ -0,0 +1,32 @@
+import './Login.css';
+import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+export class Login extends React.Component {
+
+    login= () => {
+      console.log("submitted");
+    }
+
+    render() {
+        return (
+        <div className="container">
+            <Form onSubmit={this.login}>
+                <Form.Row controlId="formBasicEmail">
+                  <Form.Label className="label">Username</Form.Label>
+                  <Form.Control type="email" placeholder="username" />
+                </Form.Row>
+
+                <Form.Row controlId="formBasicPassword">
+                  <Form.Label className="label">Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" />
+                </Form.Row>
+                <Form.Row>
+                    <Button variant="primary" type="submit">Login</Button>
+                </Form.Row>
+            </Form>
+        </div>)
+    }
+}
+
+export default Login;
